c6b819ce0c6116c50590b9118d9a1e4e1bca35d7
Loubeants Celestin <loubeants@gmail.com>
2019-05-24 18:58:11 -0400

    news-scrcrapper with screenshots

---
+++ server.js
@@ -0,0 +1,74 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.pcgamer.com/news/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $(".listingResult").each(function(i, element) {
+      var result = {};
+        console.log($(this).children("a").children("article").children("div").children("header").children("h3").text());
+        result.link= $(this)
+        .children("a")
+        .attr("href");
+
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
